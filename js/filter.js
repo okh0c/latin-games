c8fb071c427cb6bd5a8cf66750e25b97212ab4c5
choko <ckoide274@gmail.com>
2023-03-07 19:40:26 -0300

    latingames beta 1.0
    working!

--- js/filter.js
+++ js/filter.js
@@ -16,6 +16,28 @@ item[i].style.display="none";
 }
 
 
-window.onload = () => {
-  filterProduct("all");
-};
+let sortBtn = document.querySelector('.buttons').children;
+let sortItem = document.querySelector('.container-cat').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+
+
+        let targetData = this.getAttribute('data-name');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-filter') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
